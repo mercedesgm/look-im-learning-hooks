48c9ed665f0e25239bdb79a55c9d1a18747e0c64
Mercedes <mercedesgmiller92@gmail.com>
2019-11-22 14:25:51 -0500

    click and toggle light

--- src/AppClass.js
+++ src/AppClass.js
@@ -2,18 +2,45 @@ import React, {Component} from 'react';
 
 class App extends Component{
   state = {
-    count: 0
+    count: 0,
+    isOn: false
+  }
+
+  componentDidMount() {
+    document.title = `You have been clicked ${this.state.count} times.`
   }
   
+  componentDidUpdate() {
+    document.title = `You have been clicked ${this.state.count} times.`
+  }
+
   incrementCount = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }))
+  }
+
+  toggleLight = () => {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }))
   }
 
   render() {
     return (
-      <div>hello world</div>
+      <>
+        <h1>Class Version</h1>
+        <h2>Counter</h2>
+        <button onClick={this.incrementCount}>I was clicked {this.state.count} times.</button>
+
+        <h2>Toggle Light</h2>
+        <div style={{
+          height: '50px',
+          width: '50px',
+          background: this.state.isOn ? 'yellow ': 'grey'
+        }} onClick={this.toggleLight}>
+        </div>
+      </>
     );
   }
 }
--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,14 +1,38 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const App = () => {
     const [count, setCount] = useState(0)
     const incrementCount = () => {
-        setCount(count + 1)
+        setCount(prevCount => prevCount + 1)
     }
+    const [isOn, setIsOn] = useState(false)
+
+    const toggleLight = () => {
+        setIsOn(prevIsOn => !prevIsOn)
+    }
+
+    useEffect(() => {
+        document.title = `You have clicked ${count} times`
+    })
+
+
+
     return (
-        <button onClick={incrementCount}>
-            I was clicked {count} times.
-        </button>
+        <>
+            <h1>Hook Version</h1>
+            <h2>Counter</h2>
+            <button onClick={incrementCount}>
+                I was clicked {count} times.
+            </button>
+
+            <h2>Toggle Light</h2>
+            <div style={{
+            height: '50px',
+            width: '50px',
+            background: isOn ? 'yellow ': 'grey'
+            }} onClick={toggleLight}>
+            </div>
+        </>
     )
 }
 
